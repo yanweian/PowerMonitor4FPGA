b6be1774ffa1ad772f2d0e088208dd56154b76d8
yanweian <948753701@qq.com>
2018-08-01 13:51:07 +0800

    测试数据库

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,16 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> usbs=FpgaMonitor.getUsbBlaster();
+        ArrayList<String> usbs = FpgaMonitor.getUsbBlaster();
         System.out.println(usbs);
-        FpgaMonitor.executeCommand(usbs.get(0));
-        DBUtil.closeConn();
+        for(int i=0;i<usbs.size();i++){
+            int finalI = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    FpgaMonitor.executeCommand(usbs.get(finalI));
+                }
+            }).start();
+        }
     }
 }
