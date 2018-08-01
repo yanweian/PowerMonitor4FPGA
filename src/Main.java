c2d52f213ab4ef995c53fcdf675dbc9172caecd8
yanweian <948753701@qq.com>
2018-08-01 13:53:12 +0800

    测试数据库

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<String> usbs = FpgaMonitor.getUsbBlaster();
         System.out.println(usbs);
+        //对每个usb都进行监控
         for(int i=0;i<usbs.size();i++){
             int finalI = i;
             new Thread(new Runnable() {
